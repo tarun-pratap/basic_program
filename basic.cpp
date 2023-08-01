3d2ee9e1e927406f8fd7f81b0eb8a263d643e197
tarun-pratap <114357685+tarun-pratap@users.noreply.github.com>
2023-08-01 10:36:21 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    for(int i=0;i<10;i++)
+    {
+
+        for(int j=i;j<10;j++)
+
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
